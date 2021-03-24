f839f8a8e963a8b92887cac786e95eaea8db3f96
Matthew Holder <holdermatthew5@gmail.com>
2021-03-24 14:52:15 -0700

    README.md

--- braeburn_apple/set_superuser.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User
-
-user = User.objects.get(username='holdermatthew5')
-user.is_staff = True # Set to False to revoke priviliges
-user.is_admin = True # Set to False to revoke priviliges
-user.save()
